295b9d786a7d4a8bfdd05ca5eb27a28c168ec927
npyvyy <24522060@gm.uit.edu.vn>
2025-11-27 20:27:31 +0700

    Hoan thanh Lab04 - Bai01

---
+++ Lab04/Lab04/Bai01/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Bai01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btn_Get = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.BackColor = System.Drawing.SystemColors.Info;
+            this.txtUrl.Location = new System.Drawing.Point(25, 34);
+            this.txtUrl.Multiline = true;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(659, 42);
+            this.txtUrl.TabIndex = 0;
+            // 
+            // btn_Get
+            // 
+            this.btn_Get.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_Get.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Get.Location = new System.Drawing.Point(711, 36);
+            this.btn_Get.Name = "btn_Get";
+            this.btn_Get.Size = new System.Drawing.Size(118, 40);
+            this.btn_Get.TabIndex = 1;
+            this.btn_Get.Text = "GET";
+            this.btn_Get.UseVisualStyleBackColor = false;
+            this.btn_Get.Click += new System.EventHandler(this.btn_Get_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBox1.Location = new System.Drawing.Point(25, 116);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(804, 422);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(858, 573);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_Get);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btn_Get;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
