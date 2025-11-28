69d1463ef5a2c7075cc02f02ec0cb23d7ece6994
npyvyy <24522060@gm.uit.edu.vn>
2025-11-28 21:49:38 +0700

    Hoan thanh Lab04 - Bai06

---
+++ Lab04/Bai06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai06
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string tokenNhanDuoc = "";
+
+            if (args.Length > 0)
+            {
+                tokenNhanDuoc = args[0];
+            }
+            Application.Run(new UserInfoForm(tokenNhanDuoc));
+        }
+    }
+}
---
+++ Lab04/Bai06/UserInfoForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace Bai06
+{
+    public partial class UserInfoForm : Form
+    {
+        private string authToken;
+        public UserInfoForm(string token)
+        {
+            InitializeComponent();
+            this.authToken = token; 
+        }
+        public UserInfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(authToken))
+            {
+                MessageBox.Show("Chưa có Token! Vui lòng đăng nhập trước.");
+                return;
+            }
+            string url = "https://nt106.uitiot.vn/api/v1/user/me";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", authToken);
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    string responseString = await response.Content.ReadAsStringAsync();
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        JObject userData = JObject.Parse(responseString);
+                        txtUsername.Text = userData["username"]?.ToString();
+                        txtEmail.Text = userData["email"]?.ToString();
+                        txtFirstName.Text = userData["first_name"]?.ToString() ?? "";
+                        txtLastName.Text = userData["last_name"]?.ToString() ?? "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lỗi lấy thông tin: " + responseString);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi kết nối: " + ex.Message);
+                }
+            }
+        }
+    }
+}
